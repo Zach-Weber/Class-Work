a6bb125edc4a1fadb8ba311f68e04410ecb78c2b
Zach-Weber <zachary.weber@digipen.edu>
2019-10-23 17:46:57 -0700

    primes.c started and Minecraft/minecraft-BasicCommands.mkcd

---
+++ Sieve of Eratothenes/primes.c
@@ -0,0 +1,26 @@
+/* 
+Zach Weber
+23 Oct 2019
+Find a set amount of primes using Sieve of Eratothenes.
+ */
+ 
+ int main (void)
+ {
+	 
+	 void sieve(int array[], int size)
+	 {
+		 /* sets whole array to 1s */
+		 array[size] = {1};	
+		 /* sets 0 and 1 to false */
+		 array[0] = 0;
+		 array[1] = 0;
+		 
+		 int i;
+		 for (i = 0; i < 0; i++)
+		 {
+			 
+		 }
+	 }
+	 
+	 return;
+ }
