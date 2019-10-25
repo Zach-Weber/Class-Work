042b2aa6a9869309dc12d5b804ad984d664e675f
zachaweber <zach.a.weber@live.com>
2019-10-25 09:28:44 -0700

    Sieve of Eratothenes/main.c

---
+++ Sieve of Eratothenes/sieve.c
@@ -0,0 +1,18 @@
+#include <stdio.h> /* printf      */
+#include <math.h>  /* sqrt        */
+#include "sieve.h" /* TRUE, FALSE */
+
+void sieve(int array[], int size)
+{
+	
+}
+
+int twin_primes(const int primes[], int size)
+{
+  return 0;
+}
+
+double brun_constant(const int primes[], int size)
+{
+  return 0.0;
+}
