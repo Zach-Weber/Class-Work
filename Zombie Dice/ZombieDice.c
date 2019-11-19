51928143dffdc4352ba1dbd48316971ece47986f
Zach-Weber <zachary.weber@digipen.edu>
2019-11-18 17:57:01 -0800

    Commit

--- Zombie Dice/ZombieDice.c
+++ Zombie Dice/ZombieDice.c
@@ -13,6 +13,11 @@ gcc -Wall -Wextra -ansi -pedantic -O -o ZombieDice ZombieDice.c
 /*time*/
 #include <time.h>
 
+/* Prototypes */
+void RefillHand(char hand[], char bag[], int* bagCurrAmount);
+void RollDice(char hand[], int* potentialBrains, int* shots); 
+
+
 
 int main(void)
 {
@@ -20,53 +25,128 @@ int main(void)
 	char bag[13] = {'R','R','R','Y','Y','Y','Y','G','G','G','G','G','G'};
 	char hand[3] = {0};
 	
-	/* Go through the hand */
+	/* Just defining these here for now, should be put into a turn */
+	int potentialBrains = 0;
+	int shots = 0;
+	
+	RefillHand(hand, bag, &bagCurrAmount);
+	RollDice(hand, &potentialBrains, &shots);
+	
+	return 0;
+}
+
+
+
+/* Go through the hand and fill empty slots with dice from the bag */
+void RefillHand(char hand[], char bag[], int* bagCurrAmount)
+{
+	int handIndex;
+	for (handIndex = 0; handIndex < 3; ++handIndex)
 	{
-		int handIndex;
-		for(handIndex = 0; handIndex < 3; ++handIndex)
+		/* Draw from bag */
+		/* Only fill hand slots that are empty */
+		if (hand[handIndex] == 0)
 		{
-			/* Draw from bag */
-			/* Only fill hand slots that are empty */
-			if (hand[handIndex] == 0)
+			/* Pick a rand num */
+			int bagIndex = rand() % *bagCurrAmount;
+			/* Take a die from the bag */
+			hand[handIndex] = bag[bagIndex];
+			/* We took a die, so subtrant how many we have left */
+			*bagCurrAmount -= 1;
+			/* Swap the now in use die to the back of the bag array */
 			{
-				/* Pick a rand num */
-				int bagIndex = rand() % badCurrAmount;
-				/* Take a die from the bag */
-				hand[handIndex] = bag[bagIndex];
-				/* We took a die, so subtrant how many we have left */
-				bagCurrAmount -= 1;
-				/* Swap the now in use die to the back of the bag array */
-				{
-					char temp = bag[bagIndex];
-					bag[bagIndex] = bag[bagCurrAmount];
-					bag[bagCurrAmount] = temp;
-				}
+				char temp = bag[bagIndex];
+				bag[bagIndex] = bag[*bagCurrAmount];
+				bag[*bagCurrAmount] = temp;
 			}
 		}
-	} /* End fill the hand */
-	
-	/* Roll dice based on colors in hand */
-	{
-		int roll = (rand() % 6) + 1;
-		/*  */
-		switch (hand[0])
-		{
-			case 'G':
-				{
-				
-				}
-				break;
-			case 'Y':
-				{
-					
-				}
-				break;
-			case 'R':
-				{
-					
-				}
-				break;
-		}
 	}
+	return;
+} /* End fill the hand */
+
+
+
+/* Roll dice based on colors in hand */
+void RollDice(char hand[], int* potentialBrains, int* shots)
+{
+	int handIndex;
+	for (handIndex = 0; handIndex < 3; ++handIndex)
+	{
+		int roll = (rand() % 6);
+		
+		switch (hand[handIndex])
+		{
+		case 'G':
+			{
+				switch (roll)
+				{
+					case 1:
+					case 2:
+					case 3:
+						printf("[%%]"); 		/* Print a brain 						*/
+						*potentialBrains += 1;	/* Account for the score				*/
+						hand[handIndex] = 0; 	/* this slot in the hand is now open 	*/
+						break;
+					case 4:
+					case 5:
+						printf("[',]"); 		/* Print a runner 						*/
+						break;
+					case 6:
+						printf("[**]");			/* Print a shot 						*/
+						*shots += 1;				/* Account for rolling a shot			*/
+						hand[handIndex] = 0;	/* This slot in the hand is open		*/
+						break;
+				}
+			}
+			break;
+		case 'Y':
+			{
+				switch (roll)
+				{
+					case 1:
+					case 2:
+						printf("[%%]"); 		/* Print a brain 						*/
+						*potentialBrains += 1;	/* Account for the score				*/
+						hand[handIndex] = 0; 	/* this slot in the hand is now open 	*/
+						break;
+					case 3:
+					case 4:
+						printf("[',]"); 		/* Print a runner 						*/
+						break;
+					case 5:	
+					case 6:
+						printf("[**]");			/* Print a shot 						*/
+						*shots += 1;				/* Account for rolling a shot			*/
+						hand[handIndex] = 0;	/* This slot in the hand is open		*/
+						break;
+				}
+			}
+			break;
+		case 'R':
+			{
+				switch (roll)
+				{
+					case 1:
+						printf("[%%]"); 		/* Print a brain 						*/
+						*potentialBrains += 1;	/* Account for the score				*/
+						hand[handIndex] = 0; 	/* this slot in the hand is now open 	*/
+						break;
+					case 2:
+					case 3:
+					case 4:
+						printf("[',]"); 		/* Print a runner 						*/
+						break;
+					case 5:
+					case 6:
+						printf("[**]");			/* Print a shot 						*/
+						*shots += 1;				/* Account for rolling a shot			*/
+						hand[handIndex] = 0;	/* This slot in the hand is open		*/
+						break;
+				}
+			}
+			break;
+		}
+	} 
+	return;
 }
 
